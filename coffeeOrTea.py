2369247cc3e97841ce8114dd568789941b877f6c
adnan77ai <yasinadnan79@gmail.com>
2022-11-11 21:05:32 +0600

    initialized

---
+++ coffeeOrTea.py
@@ -0,0 +1,5 @@
+drink = input("Do you prefer coffee or tea?")
+if drink == "coffee":
+  print("Tea is better.")
+else:
+  print("Excellent choice.")
---
+++ self_detector.py
@@ -0,0 +1,6 @@
+myName = input("What's your name?: ")
+if myName == "Adnan":
+ print("Welcome Dude!")
+ print("You're just the coolest dude I've ever seen")
+else:
+ print("Who on earth are you?!")
